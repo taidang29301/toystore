519c3092398a601968f4261584aae230df2eb70d
taidang29301 <72535988+taidang29301@users.noreply.github.com>
2020-10-29 15:25:32 +0700

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-	$server = "localhost";
-	$username = "postgres";
-	$password = "taidang2901";
-	$database = "postgres";
+	$server = "ec2-18-210-180-94.compute-1.amazonaws.com";
+	$username = "xicxkvjiebekys";
+	$password = "c83f6f5288529b4c99f977814c51b29e669948202c40fef7e003f9dc6b136049";
+	$database = "d1ikmmnci5t1v";
 	$connString = "host=$server dbname=$database user=$username password=$password";
 ?>
